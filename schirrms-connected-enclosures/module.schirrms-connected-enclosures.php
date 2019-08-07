6766349c7779e5b34c771bc73ced3489f5cd0a07
Schirrms <schirrms@schirrms.net>
2019-08-07 16:51:32 +0200

    small corrections (version 1.1.2)

--- schirrms-connected-enclosures/module.schirrms-connected-enclosures.php
+++ schirrms-connected-enclosures/module.schirrms-connected-enclosures.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'schirrms-connected-enclosures/1.1.1',
+	'schirrms-connected-enclosures/1.1.2',
 	array(
 		// Identification
 		//
